d55dfcadcd680a0984b840905d5561a4ede480b0
Patrick <73622070+holycactusjuice@users.noreply.github.com>
2025-01-18 11:53:57 -0500

    vutftxrtdyf

---
+++ allosaurus_test.py
@@ -0,0 +1,15 @@
+from allosaurus.app import read_recognizer
+
+model = read_recognizer()
+# model = read_recognizer("eng2102")
+
+audio_file = "apricot_ryan"
+audio_file = f"audio_files/{audio_file}.wav"
+
+# res = model.recognize(audio_file, topk=5, timestamp=True)
+res = model.recognize(audio_file)
+print(res)
+
+
+# with open('test.txt', 'w', encoding='utf-8') as output_file:
+#     output_file.write(res)
